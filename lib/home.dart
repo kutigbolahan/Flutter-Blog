5d2759fb942c33093d9c188e7028c7b2e0b5131a
kuti gbolahan <kutigbolahan@ymail.com>
2020-09-03 09:19:19 +0100

    search bar

--- lib/home.dart
+++ lib/home.dart
@@ -15,123 +15,135 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Dashboard'),
         actions: [Icon(Icons.more_vert)],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 25.0, right: 25),
-            child: TextField(),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(14.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Article',
-                  style: TextStyle(fontWeight: FontWeight.w700),
-                ),
-                GestureDetector(
-                  onTap: () {},
-                  child: Text(
-                    'All Article',
-                    style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        color: Colors.teal[400],
-                        fontSize: 15),
-                  ),
-                )
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 28.0, left: 28, top: 20),
+              child: Row(
+                children: [
+                  Icon(Icons.search),
+                  Expanded(
+                      child: TextField(
+                    decoration: InputDecoration(hintText: 'Search'),
+                  ))
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 14.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: 170,
-                  height: 210,
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    elevation: 9,
-                    color: Colors.blue[900],
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Image.asset(
-                          'assets/images/tree.jpg',
-                          width: 170,
-                          height: 150,
-                          fit: BoxFit.fill,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0, top: 4.0),
-                          child: Text(
-                            'Trees',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          child: Text(
-                            'Jonathan Pie',
-                            style: TextStyle(color: Colors.white, fontSize: 12),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 170,
-                  height: 210,
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    elevation: 9,
-                    color: Colors.blue[900],
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Image.asset(
-                          'assets/images/iceland.jpg',
-                          width: 170,
-                          height: 150,
-                          fit: BoxFit.fill,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0, top: 4.0),
-                          child: Text(
-                            'Iceland',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          child: Text(
-                            'The air we breath',
-                            style: TextStyle(color: Colors.white, fontSize: 12),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+            SizedBox(
+              height: 30,
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Article',
+                    style: TextStyle(fontWeight: FontWeight.w700),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'All Article',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: Colors.teal[400],
+                          fontSize: 15),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 170,
+                    height: 210,
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      elevation: 9,
+                      color: Colors.blue[900],
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Image.asset(
+                            'assets/images/tree.jpg',
+                            width: 170,
+                            height: 150,
+                            fit: BoxFit.fill,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0, top: 4.0),
+                            child: Text(
+                              'Trees',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Text(
+                              'Jonathan Pie',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 12),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 170,
+                    height: 210,
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      elevation: 9,
+                      color: Colors.blue[900],
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Image.asset(
+                            'assets/images/iceland.jpg',
+                            width: 170,
+                            height: 150,
+                            fit: BoxFit.fill,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0, top: 4.0),
+                            child: Text(
+                              'Iceland',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Text(
+                              'The air we breath',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 12),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
